66fca8b3fb850837484630fa30a4014d7478f9ca
Cristian Gutierrez <instanciageek@gmail.com>
2022-05-15 14:26:19 -0400

    no-history: - add winston - implemented logger

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from "express";
+import Logger from './utils/logger'
 
 const app: Express = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
+  Logger.info('root path service')
   res.send('express + typescript')
 })
 
 app.listen(port, () => {
-  console.log('express up!')
+  Logger.info('express its a live!')
 })
