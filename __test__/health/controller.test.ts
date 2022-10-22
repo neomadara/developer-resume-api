4f1ca395da77637d621e98c4d2590d006171df49
Cristian Gutierrez <instanciageek@gmail.com>
2022-10-22 18:45:15 -0300

    no-history: - add test script - add supertest library - add jest - add ts-jest - health test endpoint

---
+++ __test__/health/controller.test.ts
@@ -0,0 +1,19 @@
+import request from "supertest"
+
+import app from "../../app"
+
+describe('Health Route', () => {
+  let res: any;
+  beforeAll(async () => {
+    res = await request(app).get("/healthz")
+  })
+
+  it("Should return status code 200", async () => {
+    expect(res.status).toEqual(200);
+  })
+  it("Should return and object with the health information", async () => {
+    expect(res.body.hasOwnProperty('uptime')).toBe(true)
+    expect(res.body.hasOwnProperty('message')).toBe(true)
+    expect(res.body.hasOwnProperty('timestamp')).toBe(true)
+  })
+})
